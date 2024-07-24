a636351f97b16cafb8bc2fbba07795da6084ff61
Gerald <code@gera2ld.space>
2024-07-24 11:51:03 +0800

    fix: migrate to commander

--- git-ass.ts
+++ git-ass.ts
@@ -1,50 +1,47 @@
 #!/usr/bin/env -S deno run -qA
 
-import { cac } from "https://esm.sh/cac@6.7.14";
+import { program } from "npm:commander@^12.0.0";
 import { runCommand } from "https://raw.githubusercontent.com/gera2ld/deno-lib/main/lib/cli.ts";
 
-const cli = cac("git-ass");
-cli.help();
-cli.command("").action(showHelpAndThrow);
+program.name("git-ass");
 
-// Unknown command
-cli.on("command:*", showHelpAndThrow);
+program.command("prune")
+  .description("Prune branches deleted on remote").action(async () => {
+    await runCommand(["git", "fetch", "--all", "--prune"]).spawn();
+  });
 
-cli.command("prune", "Prune branches deleted on remote").action(async () => {
-  await runCommand("git", {
-    args: ["fetch", "--all", "--prune"],
-  }).spawn();
-});
-
-cli.command("purge", "Remove fully merged local branches").action(async () => {
-  const { branches, current } = await readBranches();
-  for (const branch of branches) {
-    if (branch === current) continue;
-    const { success, stderr } = await runCommand("git", {
-      args: ["branch", "-d", branch],
-      stdout: "piped",
-      stderr: "piped",
-    }).output(false);
-    let result = "ok";
-    if (!success) {
-      result = stderr.includes(" is not fully merged.")
-        ? "not fully merged, skipping"
-        : `error: ${stderr}`;
+program.command("purge")
+  .description("Remove fully merged local branches").action(async () => {
+    const { branches, current } = await readBranches();
+    for (const branch of branches) {
+      if (branch === current) continue;
+      const { success, stderr } = await runCommand([
+        "git",
+        "branch",
+        "-d",
+        branch,
+      ], {
+        stdout: "piped",
+        stderr: "piped",
+      }).output(false);
+      let result = "ok";
+      if (!success) {
+        result = stderr.includes(" is not fully merged.")
+          ? "not fully merged, skipping"
+          : `error: ${stderr}`;
+      }
+      console.log(`- ${branch} - ${result}`);
     }
-    console.log(`- ${branch} - ${result}`);
-  }
-});
+  });
 
-cli.parse();
-
-function showHelpAndThrow() {
-  cli.outputHelp();
-  Deno.exit(1);
-}
+program.parse();
 
 async function readBranches(remote = false) {
-  const { stdout } = await runCommand("git", {
-    args: ["branch", ...(remote ? ["-r"] : [])],
+  const { stdout } = await runCommand([
+    "git",
+    "branch",
+    ...(remote ? ["-r"] : []),
+  ], {
     stdout: "piped",
   }).output();
   const lines = stdout.split("\n");
