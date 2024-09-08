278650ba8fae32d8e0aa3f4da8b070a5cf491e6f
Gerald <code@gera2ld.space>
2024-09-08 16:12:44 +0800

    fix: update deno-lib

--- git-ass.ts
+++ git-ass.ts
@@ -1,49 +1,54 @@
 #!/usr/bin/env -S deno run -qA
 
 import { program } from "npm:commander@^12.0.0";
-import { runCommand } from "https://raw.githubusercontent.com/gera2ld/deno-lib/main/lib/cli.ts";
+import { runCommand } from "https://gitlab.com/gera2ld/deno-lib/-/raw/main/lib/cli.ts";
 
 program.name("git-ass");
 
-program.command("prune")
-  .description("Prune branches deleted on remote").action(async () => {
-    await runCommand(["git", "fetch", "--all", "--prune"]).spawn();
+program
+  .command("prune")
+  .description("Prune branches deleted on remote")
+  .action(async () => {
+    await runCommand(["git", "fetch", "--all", "--prune"]);
   });
 
-program.command("purge")
-  .description("Remove fully merged local branches").action(async () => {
+program
+  .command("purge")
+  .description("Remove fully merged local branches")
+  .action(async () => {
     const { branches, current } = await readBranches();
     for (const branch of branches) {
       if (branch === current) continue;
-      const { success, stderr } = await runCommand([
-        "git",
-        "branch",
-        "-d",
-        branch,
-      ], {
-        stdout: "piped",
-        stderr: "piped",
-      }).output(false);
-      let result = "ok";
-      if (!success) {
-        result = stderr.includes(" is not fully merged.")
+      const result = await runCommand(
+        ["git", "branch", "-d", branch],
+        {},
+        {
+          ensureSuccess: false,
+          buffer: true,
+        },
+      );
+      let output = "";
+      if (!result.success) {
+        const stderr = new TextDecoder().decode(await result.stderr());
+        output = stderr.includes(" is not fully merged.")
           ? "not fully merged, skipping"
           : `error: ${stderr}`;
       }
-      console.log(`- ${branch} - ${result}`);
+      console.log(`- ${branch} - ${output}`);
     }
   });
 
 program.parse();
 
 async function readBranches(remote = false) {
-  const { stdout } = await runCommand([
-    "git",
-    "branch",
-    ...(remote ? ["-r"] : []),
-  ], {
-    stdout: "piped",
-  }).output();
+  const result = await runCommand(
+    ["git", "branch", ...(remote ? ["-r"] : [])],
+    {},
+    {
+      buffer: true,
+    },
+  );
+  const stdout = new TextDecoder().decode(await result.stdout());
   const lines = stdout.split("\n");
   const current = lines
     .find((line) => line.startsWith("* "))
